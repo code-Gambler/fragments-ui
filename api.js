b23e2e30bb9fced1273697e8df3a9402f75b42aa
Steven <spillay6@myseneca.ca>
2024-02-12 23:57:55 -0500

    added logic for test post, getall, and getid routes

--- api.js
+++ api.js
@@ -11,7 +11,7 @@ const apiUrl = process.env.API_URL || 'http://localhost:8080';
 export async function getUserFragments(user) {
   console.log('Requesting user fragments data...');
   try {
-    const res = await fetch(`${apiUrl}/v1/fragments`, {
+    const res = await fetch(`${apiUrl}/v1/fragments?expand=1`, {
       // Generate headers with the proper Authorization bearer token to pass.
       // We are using the `authorizationHeaders()` helper method we defined
       // earlier, to automatically attach the user's ID token.
@@ -25,5 +25,60 @@ export async function getUserFragments(user) {
     return data;
   } catch (err) {
     console.error('Unable to call GET /v1/fragment', { err });
+  }
+}
+
+export async function postFragments(user, type, content) {
+  console.log("Post fragments data...");
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${user.idToken}`,
+        "Content-Type": `${type}`,
+      },
+      body: `${content}`,
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log("Post user fragments data", { data });
+  } catch (err) {
+    console.error("Unable to call POST /fragment", { err });
+  }
+}
+
+export async function getFragment(user, id) {
+  console.log("Requesting user fragments data...");
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      headers: {
+        // Include the user's ID Token in the request so we're authorized
+        Authorization: `Bearer ${user.idToken}`,
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const headers = res.headers.get("content-type");
+    var data;
+    console.log("CONTENT_TYPE", headers);
+    if (headers.includes("text/plain")) {
+      data = await res.text();
+    } else if (headers.includes("application/json")) {
+      data = await res.text();
+    } else if (headers.includes("text/html")) {
+      data = await res.text();
+    } else if (headers.includes("image/")) {
+      data = await res.blob();
+      console.log("image", data);
+    } else {
+      data = await res.text();
+    }
+    console.log("Got user fragments data", { data });
+    return data;
+  } catch (err) {
+    console.error("Unable to call GET /v1/fragment", { err });
   }
 }
--- app.js
+++ app.js
@@ -1,49 +1,112 @@
 // src/app.js
 
 import { Auth, getUser } from './auth';
-import { getUserFragments } from './api';
+import { postFragments, getFragment, getUserFragments } from './api';
 
 async function init() {
-  // Get our UI elements
-  const userSection = document.querySelector('#user');
-  const loginBtn = document.querySelector('#login');
-  const logoutBtn = document.querySelector('#logout');
-  
-  // Wire up event handlers to deal with login and logout.
-  loginBtn.onclick = () => {
-      // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
-      // https://docs.amplify.aws/lib/auth/advanced/q/platform/js/#identity-pool-federation
-      Auth.federatedSignIn();
+    // Get our UI elements
+    const userSection = document.querySelector('#user');
+    const loginBtn = document.querySelector("#login");
+    const logoutBtn = document.querySelector("#logout");
+    const postButton = document.querySelector("#post");
+    const content = document.querySelector("#content");
+    const contentType = document.querySelector("#type");
+    const get_id = document.querySelector("#get_id");
+    const getById = document.querySelector("#getbyid");
+    const getButton = document.querySelector("#getall");
+    const getInfo = document.querySelector("#getInfo");
+    const infoData = document.querySelector("#content-type");
+
+    // Wire up event handlers to deal with login and logout.
+    loginBtn.onclick = () => {
+        // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
+        // https://docs.amplify.aws/lib/auth/advanced/q/platform/js/#identity-pool-federation
+        Auth.federatedSignIn();
     };
     logoutBtn.onclick = () => {
         // Sign-out of the Amazon Cognito Hosted UI (requires redirects), see:
         // https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js/#sign-out
         Auth.signOut();
     };
-    
+
     // See if we're signed in (i.e., we'll have a `user` object)
     const user = await getUser();
-    
+
     // TODO: later in the course, we will show all the user's fragments in the HTML...
     if (!user) {
         // Disable the Logout button
         logoutBtn.disabled = true;
         return;
     }
-    
+
     // Log the user info for debugging purposes
     console.log({ user });
-    
+
     // Update the UI to welcome the user
     userSection.hidden = false;
-    
+
     // Show the user's username
     userSection.querySelector('.username').innerText = user.username;
-    
+
     // Disable the Login button
     loginBtn.disabled = true;
     // Do an authenticated request to the fragments API server and log the result
-    const userFragments = await getUserFragments(user);
+    postButton.onclick = async () => {
+        let data = content.value;
+        let contentTypeValue =
+            contentType.options[contentType.selectedIndex].value;
+        await postFragments(user, contentTypeValue, data);
+    };
+    getButton.onclick = async () => {
+        // Get a fragment from the fragments API server
+        let fragmentHtml = "";
+        let fragmentList = document.querySelector(".fragmentList");
+        fragmentList.innerHTML = "";
+        getUserFragments(user).then((data) => {
+            data = data.fragments;
+            if (data.length) {
+                // Create the titles for each column and add to the table
+                let header = document.createElement("tr");
+                let headerOptions = ["Id", "Created", "Updated", "Type"];
+                for (let column of headerOptions) {
+                    let th = document.createElement("th");
+                    th.append(column);
+                    header.appendChild(th);
+                }
+                fragmentList.appendChild(header);
+
+                for (let fragment of data) {
+                    console.log("fragment", fragment);
+
+                    let tr = document.createElement("tr");
+                    let id = document.createElement("td");
+                    let created = document.createElement("td");
+                    let updated = document.createElement("td");
+                    let type = document.createElement("td");
+
+                    id.append(fragment.id);
+                    created.append(fragment.created);
+                    updated.append(fragment.updated);
+                    type.append(fragment.type);
+                    tr.append(id, created, updated, type);
+
+                    fragmentList.appendChild(tr);
+                }
+            } else {
+                let td = document.createElement("td");
+                td.append("No fragments were found");
+
+                fragmentList.append(td);
+            }
+        });
+        fragmentList.html = fragmentHtml;
+    };
+
+    getById.onclick = async () => {
+        var res = await getFragment(user, get_id.value);
+        infoData.innerHTML = res;
+    };
+
 }
 
 // Wait for the DOM to be ready, then start the app
